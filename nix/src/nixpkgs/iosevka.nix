8d5f5e39ee9ba6d3e0920d78be8418e124a7c398
ftzm <matthew@fitzsimmons.io>
2019-11-12 22:39:51 +0100

    cooler dollar sign

--- nix/src/nixpkgs/iosevka.nix
+++ nix/src/nixpkgs/iosevka.nix
@@ -1,38 +1,35 @@
 { pkgs, ... }:
 
-let
-  iosevkaLig = (pkgs.iosevka.override {
-    privateBuildPlan = {
-      weights.regular = {
-        shape = 500;
-        menu = 500;
-        css = 500;
-      };
-      weights.medium = {
-        shape = 600;
-        menu = 600;
-        css = 600;
-      };
-      weights.semibold = {
-        shape = 600;
-        menu = 600;
-        css = 600;
-      };
-      weights.bold = {
-        shape = 700;
-        menu = 700;
-        css = 700;
-      };
-      design =
-        [
-          "v-asterisk-low"
-          "v-percent-dots"
-        ];
-      family = "Iosevka Lig";
+pkgs.iosevka.override {
+  privateBuildPlan = {
+    weights.regular = {
+      shape = 500;
+      menu = 500;
+      css = 500;
     };
-
-    set = "ftzm";
-    extraParameters = builtins.readFile ./iosevka.toml;
-  });
-in
-iosevkaLig
+    weights.medium = {
+      shape = 600;
+      menu = 600;
+      css = 600;
+    };
+    weights.semibold = {
+      shape = 600;
+      menu = 600;
+      css = 600;
+    };
+    weights.bold = {
+      shape = 700;
+      menu = 700;
+      css = 700;
+    };
+    design =
+      [
+        "v-asterisk-low"
+        "v-percent-dots"
+        "v-dollar-open"
+      ];
+    family = "Iosevka Lig";
+  };
+  set = "ftzm";
+  extraParameters = builtins.readFile ./iosevka.toml;
+}
