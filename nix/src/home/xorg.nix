c5dda4c833cfa71369b395fe00445bbbfd7a2515
ftzm <matthew@fitzsimmons.io>
2020-03-29 23:15:59 +0200

    nixpkxgs update and add alacritty

--- nix/src/home/xorg.nix
+++ nix/src/home/xorg.nix
@@ -24,17 +24,17 @@ in {
         format = ''
           <b>%s</b>
           %b'';
-        alignment = "left";
-        show_age_threshold = 60;
-        word_wrap = "yes";
-        ellipsize = "middle";
-        ignore_newline = "no";
-        stack_duplicates = "true";
-        hide_duplicate_count = false;
-        show_indicators = "yes";
-        icon_position = "off";
-        sticky_history = "yes";
-        history_length = 20;
+          alignment = "left";
+          show_age_threshold = 60;
+          word_wrap = "yes";
+          ellipsize = "middle";
+          ignore_newline = "no";
+          stack_duplicates = "true";
+          hide_duplicate_count = false;
+          show_indicators = "yes";
+          icon_position = "off";
+          sticky_history = "yes";
+          history_length = 20;
       };
       urgency_low = {
         background = "#222222";
@@ -54,6 +54,7 @@ in {
       };
     };
   };
+<<<<<<< HEAD:nix/src/home/xorg.nix
   programs.urxvt = {
     enable = true;
     fonts = [
@@ -91,28 +92,110 @@ in {
     "*color15" = "#ebdbb2";
   } // (if isNixos then { } else { "Xft.dpi" = "180"; });
   xsession = {
+=======
+  programs.alacritty = {
+>>>>>>> nixpkxgs update and add alacritty:nix/src/nixpkgs/xorg.nix
     enable = true;
-    windowManager = {
-      xmonad = {
-        enable = true;
-        enableContribAndExtras = true;
+    settings = {
+      font = {
+        normal = {
+          family = "Iosevka";
+          style = "Medium";
+        };
+        bold = {
+          family = "Iosevka";
+          style = "Bold";
+        };
+        size = 10.0;
+      };
+      colors = {
+        primary = {
+          background = "0x282828";
+          foreground = "0xebdbb2";
+        };
+        normal = {
+          black =   "0x282828";
+          red =     "0xcc241d";
+          green =   "0x98971a";
+          yellow =  "0xd79921";
+          blue =    "0x458588";
+          magenta = "0xb16286";
+          cyan =    "0x689d6a";
+          white =   "0xa89984";
+        };
+        bright = {
+          black =   "0x928374";
+          red =     "0xfb4934";
+          green =   "0xb8bb26";
+          yellow =  "0xfabd2f";
+          blue =    "0x83a598";
+          magenta = "0xd3869b";
+          cyan =    "0x8ec07c";
+          white =   "0xebdbb2";
+        };
       };
     };
-    initExtra = ''
-      export LOCALE_ARCHIVE_2_27="$(nix-build --no-out-link "<nixpkgs>" -A glibcLocales)/lib/locale/locale-archive"
-      ${pkgs.hsetroot}/bin/hsetroot -solid "#282828" &
-      export FONT_SIZE=${font_size}
-      # For non-broken locale on non-nixos
-      xsetroot -cursor_name left_ptr
-      # Mainly for ubuntu
-      export XCURSOR_PATH=$HOME/.nix-profile/share/icons:$XCURSOR_PATH
-    '';
-    pointerCursor = {
-      package = pkgs.vanilla-dmz;
-      name = "Vanilla-DMZ";
-    } // (if isNixos then { } else { size = 64; });
   };
-  qt.enable = true;
-  gtk.enable = true;
-  fonts.fontconfig.enable = true;
+  programs.urxvt = {
+      enable = true;
+      fonts = [
+          #"xft:Iosevka Lig:Medium:size=${font_size}"
+          "xft:Source Code Pro:Regular:size=${font_size}"
+          #"xft:DejaVu Sans Mono:Regular:size=${font_size}"
+        ];
+        extraConfig = {
+            scrollBar = false;
+            letterSpace = ".5";
+            smoothResize = true;
+            interalBorder = 5;
+            urgentOnBell = true;
+            #boldFont = "xft:Iosevka Lig:Medium:size=${font_size}";
+            boldFont = "xft:Source Code Pro:Regular:size=${font_size}";
+          };
+        };
+        xresources.properties = {
+          "*background" = "#282828";
+          "*foreground" = "#ebdbb2";
+          "*color0" = "#282828";
+          "*color8" = "#928374";
+          "*color1" = "#cc241d";
+          "*color9" = "#fb4934";
+          "*color2" = "#98971a";
+          "*color10" = "#b8bb26";
+          "*color3" = "#d79921";
+          "*color11" = "#fabd2f";
+          "*color4" = "#458588";
+          "*color12" = "#83a598";
+          "*color5" = "#b16286";
+          "*color13" = "#d3869b";
+          "*color6" = "#689d6a";
+          "*color14" = "#8ec07c";
+          "*color7" = "#a89984";
+          "*color15" = "#ebdbb2";
+        } // (if isNixos then { } else { "Xft.dpi" = "180"; });
+        xsession = {
+          enable = true;
+          windowManager = {
+            xmonad = {
+              enable = true;
+              enableContribAndExtras = true;
+            };
+          };
+          initExtra = ''
+            export LOCALE_ARCHIVE_2_27="$(nix-build --no-out-link "<nixpkgs>" -A glibcLocales)/lib/locale/locale-archive"
+            ${pkgs.hsetroot}/bin/hsetroot -solid "#282828" &
+            export FONT_SIZE=${font_size}
+            # For non-broken locale on non-nixos
+            xsetroot -cursor_name left_ptr
+            # Mainly for ubuntu
+            export XCURSOR_PATH=$HOME/.nix-profile/share/icons:$XCURSOR_PATH
+          '';
+          pointerCursor = {
+            package = pkgs.vanilla-dmz;
+            name = "Vanilla-DMZ";
+          } // (if isNixos then { } else { size = 64; });
+        };
+        qt.enable = true;
+        gtk.enable = true;
+        fonts.fontconfig.enable = true;
 }
