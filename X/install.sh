eff2b02bac7934f0a600d7063a96b05a430d0706
ftzm <m@ftzm.org>
2021-08-29 20:34:08 +0200

    remove unused dots

--- X/install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-ln -s ~/.dots/X/.xinitrc ~/.xinitrc
-ln -s ~/.dots/X/.Xresources ~/.Xresources
-xrdb ~/.Xresources
--- oh-my-zsh/install.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-ln -s ~/.dots/oh-my-zsh/.oh-my-zsh ~/.oh-my-zsh
-
