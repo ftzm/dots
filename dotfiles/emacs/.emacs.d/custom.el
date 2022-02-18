b86ae7f50cc6b255d66425b3b1eee06ee700561d
ftzm <m@ftzm.org>
2022-02-18 21:55:12 +0100

    delete undesirable org formatting

--- dotfiles/emacs/.emacs.d/custom.el
+++ dotfiles/emacs/.emacs.d/custom.el
@@ -9,7 +9,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-block-begin-line ((t (:inherit font-lock-comment-face :background nil))))
- '(org-block-end-line ((t (:inherit font-lock-comment-face :background nil))))
- '(org-level-2 ((t (:foreground nil :inherit org-level-1))))
- '(org-level-3 ((t (:foreground nil :inherit org-level-1)))))
+ )
