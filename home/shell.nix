7568e3ec96030473b6e455308eb84846f95f8b0f
ftzm <m@ftzm.org>
2022-05-07 22:18:26 +0200

    switch to bash, vterm fixes

--- home/shell.nix
+++ home/shell.nix
@@ -1,48 +1,50 @@
 { pkgs, config, ... }:
 
 let
-  vterm_printf = builtins.readFile ./vterm_printf.sh;
-  vterm_prompt_end = builtins.readFile ./vterm_prompt_end.sh;
+  emacs-vterm-bash = builtins.readFile ./emacs-vterm-bash.sh;
 in
 {
   programs.zsh = {
     enable = true;
-    plugins = [
-      {
-        name = "bashmarks";
-        file = "bashmarks.sh";
-        src = pkgs.fetchFromGitHub {
-          owner = "huyng";
-          repo = "bashmarks";
-          rev = "342169ad48954ac24cb58ef479a6d3238e90f42f";
-          sha256 = "1p7yd0vd8his8lf8jzn8xiv3cwiw0yswk94744jk7pdn5znfv5gp";
-        };
-      }
-      {
-        name = "zsh-syntax-highlighting";
-        src = pkgs.fetchFromGitHub {
-          owner = "zsh-users";
-          repo = "zsh-syntax-highlighting";
-          rev = "650dd79d86f885f8802732e3748d8719e787d22f";
-          sha256 = "11hynfzyv9ri7rjymj1ix016r8yqx2am692bjhq6qqdii6l8m40r";
-        };
-      }
-    ];
+    # plugins = [
+    #   {
+    #     name = "bashmarks";
+    #     file = "bashmarks.sh";
+    #     src = pkgs.fetchFromGitHub {
+    #       owner = "huyng";
+    #       repo = "bashmarks";
+    #       rev = "342169ad48954ac24cb58ef479a6d3238e90f42f";
+    #       sha256 = "1p7yd0vd8his8lf8jzn8xiv3cwiw0yswk94744jk7pdn5znfv5gp";
+    #     };
+    #   }
+    #   {
+    #     name = "zsh-syntax-highlighting";
+    #     src = pkgs.fetchFromGitHub {
+    #       owner = "zsh-users";
+    #       repo = "zsh-syntax-highlighting";
+    #       rev = "650dd79d86f885f8802732e3748d8719e787d22f";
+    #       sha256 = "11hynfzyv9ri7rjymj1ix016r8yqx2am692bjhq6qqdii6l8m40r";
+    #     };
+    #   }
+    # ];
     oh-my-zsh = {
-      enable = true;
+      # enable = true;
       plugins = [ "vi-mode" ];
       theme = "robbyrussell";
     };
-    enableAutosuggestions = true;
-    enableCompletion = true;
+    # enableAutosuggestions = true;
+    # enableCompletion = true;
     shellAliases = {
       ".." = "cd ..";
       ll = "ls -l";
       k = "kubectl";
     };
-    history.ignoreDups = true;
-    history.extended = true;
+    # history.ignoreDups = true;
+    # history.extended = true;
     initExtra = ''
+      # for profilgin
+      zmodload zsh/zprof
+
       EDITOR='vim'
       export PATH=$HOME/bin:$HOME/.local/bin:$PATH
       # Ask for passwords in cli
@@ -51,29 +53,58 @@ in
       export GUIPASS=${pkgs.lxqt.lxqt-openssh-askpass}/bin/lxqt-openssh-askpass
       # unalias conflicting aliases for bashmarks
       unalias l
-      [[ -f ~/.falcon ]] && source ~/.falcon
       # Go
       export GOPATH=$HOME/.go
       export PATH=$GOPATH/bin:$PATH
-      ${vterm_printf}
-      ${vterm_prompt_end}
 
       alias groot='cd $(git rev-parse --show-toplevel)'
 
     '' + config.personal.zsh_extra;
   };
+  programs.bash = {
+    enable = true;
+    bashrcExtra = ''
+
+      export SHELL=bash
+
+      export PS1='\[\e[1;$(($?==0?32:91))m\]\w\a\] $ \[\e[0m\]'
+
+      # write to history immediately
+      shopt -s histappend
+      export PROMPT_COMMAND="''${PROMPT_COMMAND:+$PROMPT_COMMAND; }"'history -a'
+
+      EDITOR='vim'
+      export PATH=$HOME/bin:$HOME/.local/bin:$PATH
+
+      # Ask for passwords in cli
+      unset SSH_ASKPASS
+      unset GIT_ASKPASS
+      export GUIPASS=${pkgs.lxqt.lxqt-openssh-askpass}/bin/lxqt-openssh-askpass
+
+      ${emacs-vterm-bash}
+    '';
+  };
   programs.fzf = {
     enable = true;
     enableZshIntegration = true;
+    enableBashIntegration = true;
     defaultCommand = "ag -g ''";
     defaultOptions = [
-      "--color fg:223,hl:166,fg+:235,bg+:4,hl+:223"
-      "--color info:246,prompt:223,spinner:142,pointer:166,marker:166 "
+      "--color fg:223,hl:166,bg+:\#282828,fg+:223,hl+:\#fabd2f"
+      "--color info:\#504945,prompt:223,spinner:142,pointer:166,marker:166 "
     ];
   };
+  # programs.starship = {
+  #   enable = true;
+  #   enableBashIntegration = true;
+  #   settings = {
+  #     add_newline = false;
+  #   };
+  # };
   programs.direnv = {
     enable = true;
-    enableZshIntegration = true;
+    enableBashIntegration = true;
+    # enableZshIntegration = true;
     nix-direnv.enable = true;
   };
 }
