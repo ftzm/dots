cf4c03e9b827371b946ff298dd60b97f5e58d9ac
ftzm <m@ftzm.org>
2022-06-03 08:02:45 +0200

    add dired-launch

--- dotfiles/emacs/.emacs.d/modules/init-dired.el
+++ dotfiles/emacs/.emacs.d/modules/init-dired.el
@@ -8,6 +8,12 @@
   (setq dired-omit-files "^\\.?#\\|^\\.$\\|^\\.\\.$\\|^\\..*$")
   )
 
+(use-package dired-launch
+  :straight t
+  :hook (dired-mode . dired-launch-mode)
+  :config
+  (define-key dired-launch-mode-map (kbd "C-c C-c") 'dired-launch-command))
+
 
 (setq dired-listing-switches "-aBhl  --group-directories-first")
 
